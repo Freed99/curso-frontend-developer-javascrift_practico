36ffcaf1ffaa934d4bffd345a36605125df9e34b
freed99 <femarquezm@uqvirtual.edu.co>
2022-09-18 21:17:39 -0500

    Integración de la clase8 al inde archivo html, css y js

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const menuNivel = document.querySelector('.navbar-email');
 const desktopmenu = document.querySelector('.desktop-menu');
+const menuMobil = document.querySelector('.menu');
+const mobilMenu = document.querySelector('.mobile-menu');
+
 
 menuNivel.addEventListener('click', toggleDesktopmenu);
 
+menuMobil.addEventListener('click', toggleMobilmenu);
+
 function toggleDesktopmenu() {
     desktopmenu.classList.toggle('inactive');
+}
+
+function toggleMobilmenu() {
+    mobilMenu.classList.toggle('inactive');
 }
