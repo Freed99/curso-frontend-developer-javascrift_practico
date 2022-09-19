1248b3940782c2a959833bfd5b8b71258ea25b75
freed99 <femarquezm@uqvirtual.edu.co>
2022-09-19 16:39:49 -0500

    Integración de la clase13 al inde archivo html, css y js

--- main.js
+++ main.js
@@ -2,16 +2,27 @@ const menuNivel = document.querySelector('.navbar-email');
 const desktopmenu = document.querySelector('.desktop-menu');
 const menuMobil = document.querySelector('.menu');
 const mobilMenu = document.querySelector('.mobile-menu');
-
+const navbarCart = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
 
 menuNivel.addEventListener('click', toggleDesktopmenu);
 
-menuMobil.addEventListener('click', toggleMobilmenu);
-
 function toggleDesktopmenu() {
     desktopmenu.classList.toggle('inactive');
+    productDetail.classList.add('inactive');
 }
+
+menuMobil.addEventListener('click', toggleMobilmenu);
 
 function toggleMobilmenu() {
     mobilMenu.classList.toggle('inactive');
+    productDetail.classList.add('inactive');
+}
+
+navbarCart.addEventListener('click', toggleNavbarCart);
+
+function toggleNavbarCart() {
+    productDetail.classList.toggle('inactive');
+    mobilMenu.classList.add('inactive');
+    desktopmenu.classList.add('inactive');
 }
