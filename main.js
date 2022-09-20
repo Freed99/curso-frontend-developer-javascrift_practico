58da65d44155ca2b78632ee42b476e52b0b2befe
freed99 <femarquezm@uqvirtual.edu.co>
2022-09-20 15:40:13 -0500

    Se agregarón los productos usando js basados en la clase6, con su respectivo html y css

--- main.js
+++ main.js
@@ -26,3 +26,72 @@ function toggleNavbarCart() {
     mobilMenu.classList.add('inactive');
     desktopmenu.classList.add('inactive');
 }
+
+const productList = [];
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Car',
+    price: 1500,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Tax',
+    price: 1000,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+{/* <div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+    <div class="product-info">
+    <div>
+        <p>$120,00</p>
+        <p>Bike</p>
+    </div>
+    <figure>
+        <img src="./icons/bt_add_to_cart.svg" alt="">
+     </figure>
+</div> */}
+
+function renderList(arr){
+for (product of arr) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', product.img);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText= '$' + product.price;
+
+    const productName = document.createElement('div');
+    productName.innerText= product.name;
+
+    productInfoDiv.append(productPrice, productName);
+
+    const figure = document.createElement('figure');
+
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg')
+
+    figure.append(productImgCart);
+
+    productInfo.append(productInfoDiv, figure);
+
+    productCard.append(img, productInfo);
+
+    const cardsContainer = document.querySelector('.cards-container')
+    cardsContainer.append(productCard);
+}
+}
+
+renderList(productList);
